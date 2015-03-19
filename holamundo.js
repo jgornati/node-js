d662403d9d668e8ffea785d667f76111dbdf8469
jgornati <joelgornati@gmail.com>
2015-03-19 01:33:35 -0300

    hola lo subi desde el cloud

--- holamundo.js
+++ holamundo.js
@@ -3,6 +3,6 @@ var http = require('http');
         http.createServer(function (req, res) {
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end('Hola Mundo\n');
-        }).listen(8080, '127.0.0.1');
+        }).listen(8080, '0.0.0.0');
         
-        console.log('Server running at http://127.0.0.1:8080/')
+        console.log('Server running at http://179.43.126.244:8080/')
