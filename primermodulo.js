986b54541c90738049cead7515c741e5cab56cf4
jgornati <joelgornati@gmail.com>
2015-03-19 09:52:22 -0300

    tutorial

---
+++ primermodulo.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+function iniciar(){
+	function onRequest(request, response){
+	  console.log("Peticion Recibida.");
+	  response.writeHead(200, {"Content-Type": "text/html"});
+	  response.write("hola mundo");
+	  response.end();
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Servidor Iniciado.");
+}
+
+exports.iniciar = iniciar;
+
+
